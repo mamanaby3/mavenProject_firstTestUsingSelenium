a1bcc662b4e37a333a92523d14cc3e204e05200d
Ismael SACKO <sakoismael.is.1999@gmail.com>
2024-01-22 20:23:15 +0000

    Initialisation du projet

---
+++ src/main/java/com/framwork/mavenProject_firstTestUsingSelenium/MavenProjectFirstTestUsingSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.framwork.mavenProject_firstTestUsingSelenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjectFirstTestUsingSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProjectFirstTestUsingSeleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/framwork/mavenProject_firstTestUsingSelenium/MavenProjectFirstTestUsingSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framwork.mavenProject_firstTestUsingSelenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectFirstTestUsingSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
